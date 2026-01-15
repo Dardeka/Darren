0a5515dc5fb64ff5b40f9dad6a8916b233b0e38c
Dardeka <ddkwari2@gmail.com>
2026-01-15 22:36:03 +0700

    feat: update contact section with button links for social media

--- src/sections/contact.jsx
+++ src/sections/contact.jsx
@@ -1,23 +1,35 @@
+import { Button } from "../components/ui/button";
+
 function Contact() {
     return(
         <section id="Contact" className="pt-[150px] mb-[150px] flex flex-col items-center" data-aos="fade-up" data-aos-duration="750">
             <h2 className="text-3xl font-bold">Hubungi saya via :</h2>
-            <div className="flex flex-row mt-20 gap-20 items-center">
-                <a href="www.linkedin.com/in/darren-dk" className="flex flex-row items-center gap-5 text-xl font-bold">
-                    <img src="/linkedin.png" alt="" width="35px"/>
-                    Darren Dharmasaputra Kwariawan
-                </a>
-                <a href="www.linkedin.com/in/darren-dk" className="flex flex-row items-center gap-5 text-xl font-bold">
-                    <img src="/github.png" alt="" width="35px"/>
-                    Dardeka
-                </a>
-                <a href="www.linkedin.com/in/darren-dk" className="flex flex-row items-center gap-5 text-xl font-bold">
-                    <img src="/gmail.png" alt="" width="35px"/>
-                    ddkwarii@gmail.com
-                </a>
+            <div className="flex flex-row mt-10 py-7 gap-20 items-center" >
+                <div>
+                    <Button onClick={() => handleClick("https://www.linkedin.com/in/darren-dk")} className="!bg-transparent !text-black flex flex-row items-center gap-5 py-7 text-xl font-bold cursor-pointer rounded-none border-b-0 border-transparent hover:border-b-2 hover:border-gray-500" data-aos="fade-up" data-aos-duration="750">
+                        <img src="/linkedin.png" alt="" width="35px"/>
+                        Darren Dharmasaputra Kwariawan
+                    </Button>
+                </div>
+                <div className="">
+                    <Button onClick={() => handleClick("https://github.com/Dardeka")} className="!bg-transparent !text-black flex flex-row items-center gap-5 py-7 text-xl font-bold cursor-pointer border-b-2 border-transparent rounded-none border-b-0 border-transparent hover:border-b-2 hover:border-gray-500" data-aos="fade-up" data-aos-duration="1000">
+                        <img src="/github.png" alt="" width="35px"/>
+                        Dardeka
+                    </Button>
+                </div>
+                <div className="">
+                    <Button onClick={() => handleClick("https://mail.google.com/mail/u/0/#inbox?compose=CllgCKCCSDqXkXtBvQhswtzkrHcTfndfwkVTPdXMCCxkXVsmmWsJdmKknBwNqlCXSbdcJTbBNpL")} className="!bg-transparent !text-black flex flex-row items-center gap-5 py-7 text-xl font-bold cursor-pointer rounded-none border-b-0 border-transparent hover:border-b-2 hover:border-gray-500" data-aos="fade-up" data-aos-duration="1250">
+                        <img src="/gmail.png" alt="" width="35px"/>
+                        ddkwarii@gmail.com
+                    </Button>
+                </div>
             </div>
         </section>
     )
+}
+
+function handleClick(props){
+    window.open(props, '_blank');
 }
 
 export default Contact;
